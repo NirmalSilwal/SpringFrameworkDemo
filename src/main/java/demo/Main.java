4bf95c6bc5e86779c01dabd18135d33156b6de5c
NirmalSilwal <nirmaldeepsil.11@gmail.com>
2026-04-14 18:34:17 -0500

    instantiation using spring beans xml path

--- src/main/java/demo/Main.java
+++ src/main/java/demo/Main.java
@@ -1,8 +1,23 @@
 package demo;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+
+        // traditional way
+//        Doctor doctor = new Doctor();
+//        doctor.assist();
+
+        // using beans
+        Doctor doctor = context.getBean(Doctor.class);
+        doctor.assist();
+
+        Nurse nurse = context.getBean(Nurse.class);
+        nurse.assist();
     }
 }
